cd525fd809596b407ef7605cb43e32089acc5c45
Céline Desurmont <cdesurmo@gmail.com>
2020-12-14 18:12:52 +0100

    UPDATE About page : added the app rules

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-container>
+      <v-row>
+        <v-col cols="12" offset-md="2" md="8">
+          <v-card class="profile-card mt-5 pb-5">
+            <h1 class="text-h5 pt-5 text-center">Règlement</h1>
+            <p class="subheading font-weight-regular mt-5 mx-5">
+              Vous pouvez publier des messages seuls ou accompagnés d'une image.
+              <br>
+              Vous pouvez ajouter une photo de profil et renseigner votre bio sur la page de votre profil en cliquant sur votre avatar dans le header une fois connecté.
+              <br>
+              Merci de rester bienveillant envers vos collègues. Tout manquement au respect du règlement pourra entraîner
+              la suppression d'un commentaire, d'un post, voire même d'un compte.
+              <br>
+              L'Admin veille pour maintenir la bonne ambiance ! 😊
+              <br>
+            </p>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
